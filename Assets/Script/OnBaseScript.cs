60945b12fdaabf19c264d238ac63ef600167d977
Daniel <danilzagor@gmail.com>
2022-06-05 11:41:50 +0200

    Переделан invoke OxygenSystem. Сделан Trail Effect

--- Assets/Script/OnBaseScript.cs
+++ Assets/Script/OnBaseScript.cs
@@ -18,7 +18,6 @@ public class OnBaseScript : MonoBehaviour
         if (other == player.boxCollider) //Checking what object triggered the collider and if it is player, changing bool
         {
             player.PlayerIsAtBase = false;
-            Debug.Log("");
         }
     }
 }
--- Assets/Script/player.cs
+++ Assets/Script/player.cs
@@ -14,16 +14,19 @@ public class player : MonoBehaviour
     private RaycastHit2D hit;
     private SpriteRenderer sprite;
     [SerializeField] private Sprite[] playeranimationsprites = new Sprite[3];
-    [SerializeField] private float PlayerHealth;
-    [SerializeField] private float PlayerOxygen;
+    [SerializeField] private int PlayerHealth;
+    [SerializeField] private int PlayerOxygen;
     [SerializeField] private Text hptext;
     [SerializeField] private Text oxygentext;
+    [SerializeField] private TrailRenderer traileffect;
     void Start()
     {
         sprite = GetComponent<SpriteRenderer>();
         boxCollider = GetComponent<CapsuleCollider2D>();
         PlayerHealth = 100;
-        PlayerOxygen= 100;
+        PlayerOxygen = 100;
+
+
     }
     private void FixedUpdate()
     {
@@ -31,46 +34,53 @@ public class player : MonoBehaviour
         oxygentext.text = "oxygen:" + PlayerOxygen;//for debug
         PlayerMovement();
         OxygenSystem();
-        
+
     }
     private void OxygenSystem()
     {
         if (PlayerIsAtBase == true)
         {
-            InvokeRepeating("OxygenAtBase", 0f, 1f);
+            InvokeRepeating("OxygenAtBase", 1f, 1f);
             CancelInvoke("OxygenAtSpace");
+            traileffect.enabled = false;
         }
         else if (PlayerIsAtBase == false)
         {
-            InvokeRepeating("OxygenAtSpace", 0f, 100f);
+            InvokeRepeating("OxygenAtSpace", 1f, 1);
             CancelInvoke("OxygenAtBase");
+            traileffect.enabled = true;
         }
-        
+
     }
     private void PlayerMovement()
     {
         float x = joystick.Horizontal;
         float y = joystick.Vertical;
-        
+
         moveDelta = new Vector3(x, y, 0);
         if (moveDelta.y > Math.Abs(moveDelta.x))
         {
+            traileffect.transform.position = new Vector3(transform.position.x, transform.position.y, -2);
             sprite.sprite = playeranimationsprites[2];
         }
         else
         if (moveDelta.x == 0 && moveDelta.y == 0)
         {
             sprite.sprite = playeranimationsprites[1];
-        }else
+            traileffect.transform.position = new Vector3(transform.position.x, transform.position.y, 1);
+        }
+        else
         if (moveDelta.x > 0)
         {
             sprite.sprite = playeranimationsprites[0];
             boxCollider.transform.localScale = Vector3.one;
+            traileffect.transform.position = new Vector3(transform.position.x, transform.position.y, 1);
         }
         else if (moveDelta.x < 0)
         {
             sprite.sprite = playeranimationsprites[0];
             boxCollider.transform.localScale = new Vector3(-1, 1, 1);
+            traileffect.transform.position = new Vector3(transform.position.x, transform.position.y, 1);
         }
         hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0,
             new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
@@ -88,16 +98,18 @@ public class player : MonoBehaviour
     void OxygenAtBase()
     {
         if (PlayerOxygen < 100) PlayerOxygen++;
+        CancelInvoke("OxygenAtBase");
     }
     void OxygenAtSpace()
     {
-        if (PlayerOxygen > 0) PlayerOxygen-=0.01f;
-        if (PlayerOxygen <= 0) PlayerHealth -= 0.01f;
+        if (PlayerOxygen > 0) PlayerOxygen -= 1;
+        if (PlayerOxygen <= 0) PlayerHealth -= 1;
         if (PlayerHealth <= 0)
         {
             transform.localPosition = new Vector3(0, 0, -1);
             PlayerOxygen = 100;
             PlayerHealth = 100;
         }
+        CancelInvoke("OxygenAtSpace");
     }
 }
