525c30f90147dc5e407c877f5b7e7def1b919f65
Daniel <danilzagor@gmail.com>
2023-04-14 22:43:33 +0200

    l

--- Assets/Script/Stukabat.cs
+++ Assets/Script/Stukabat.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class Stukabat : Enemy
 {
     [SerializeField] private float RotateSpeed = 0.05f;
-    [SerializeField] private float Radius = 3f;
+    [SerializeField] private float Radius = 1.5f;
     private Vector2 _centre;
     private float _angle;
+    bool isInScream=false;
+    protected override void Start()
+    {
+        base.Start();       
+    }
     protected override void FixedUpdate()
     {
-        /*direction = playerTransform.position - transform.position;
+        direction = playerTransform.position - transform.position;
         if (direction.magnitude <= distanceToAttack)
         {
 
@@ -22,19 +27,34 @@ public class Stukabat : Enemy
             }
 
             if (direction.magnitude <= 3)
+            {                            
+                Invoke("ScreamAttack", 5f);
+            }
+            if (isInScream==false && direction.magnitude <= 10)
             {
                 MakeACircle();
             }
-        }*/
-        MakeACircle();
+        }
     }
     private void MakeACircle()
     {
         _centre = player.PlayerGameObject.transform.position;
-        _angle += RotateSpeed * Time.deltaTime;
-        transform.eulerAngles += new Vector3(0, 0, RotateSpeed * Time.deltaTime);
-        gameObject.transform.rotation = Quaternion.AngleAxis(_angle * 10, Vector3.forward);
-        var offset = new Vector2(Mathf.Sin(_angle * 10 /20), Mathf.Cos(_angle)) * Radius;
+        _angle -= RotateSpeed * Time.deltaTime;
+        transform.eulerAngles += new Vector3(0, 0, RotateSpeed * Time.deltaTime*50+180);
+        var offset = new Vector2(Mathf.Sin(_angle), Mathf.Cos(_angle)) * Radius;
         transform.position = _centre + offset;
+    }
+    private void ScreamAttack()
+    {
+        isInScream = true;
+        CancelInvoke("ScreamAttack");
+        GetComponent<Animator>().Play("StukabatScream");
+        player.PlayerHealth -= 10;
+        GetComponent<Animator>().Play("StukabatFly");
+        Invoke("BoolIsInScream", 0.5f);
+    }
+    private void BoolIsInScream()
+    {
+        isInScream = false;
     }
 }
